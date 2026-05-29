f2791a9340c8d28387bf721b70185534b1b84d5b
shashanks19102010-hue <shashanks19102010@gmail.com>
2026-05-29 13:25:39 +0530

    Create src/components/TypingLoader.tsx

---
+++ src/components/TypingLoader.tsx
@@ -0,0 +1,15 @@
+export default function TypingLoader() {
+
+  return (
+
+    <div className="bg-[#1f1f1f] px-4 py-3 rounded-2xl flex gap-2 w-fit">
+
+      <div className="w-2 h-2 rounded-full bg-white animate-bounce"></div>
+
+      <div className="w-2 h-2 rounded-full bg-white animate-bounce"></div>
+
+      <div className="w-2 h-2 rounded-full bg-white animate-bounce"></div>
+
+    </div>
+  );
+}
